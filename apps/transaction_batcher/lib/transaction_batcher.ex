e16d166ca322e267993d80b524fa2db98d5efa3e
Andrew Lewin <andrew.lewin@divvypay.com>
2023-08-23 18:11:33 -0600

    finish transaction_batcher

--- apps/transaction_batcher/lib/transaction_batcher.ex
+++ apps/transaction_batcher/lib/transaction_batcher.ex
@@ -1,6 +1,27 @@
 defmodule TransactionBatcher do
+  alias AMQP.{Basic, Channel, Connection, Queue}
+
+  require Logger
+
+  @queue_name "transaction_batch_closed"
+
   def read_csv do
-    # Implement your function here.
-    # The transactions.csv is located at the relative path ../../../transactions.csv
+    {:ok, connection} = Connection.open()
+    {:ok, channel} = Channel.open(connection)
+    Queue.declare(channel, @queue_name)
+
+    "../../../transactions.csv"
+    |> Path.expand(__DIR__)
+    |> File.stream!()
+    |> CSV.decode(headers: true)
+    |> Enum.each(fn
+      {:ok, transaction_row} ->
+        Basic.publish(channel, "", @queue_name, Jason.encode!(transaction_row))
+
+      error ->
+        Logger.error("Error while trying to read csv row: #{inspect(error)}")
+    end)
+
+    AMQP.Connection.close(connection)
   end
 end
